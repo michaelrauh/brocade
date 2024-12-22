25fa62f30a221003ec15e1b06981372e439c77fd
michaelrauh <michaelrauh@ymail.com>
2024-12-22 16:52:49 -0500

    update counter order

--- lib/counter.ex
+++ lib/counter.ex
@@ -6,12 +6,11 @@ defmodule Counter do
   end
 
   def increment(%Counter{shape: shape, options: options}) do
-    IO.inspect(options)
 
     case List.pop_at(options, 0) do
       {nil, []} ->
         new_shape = [2 | shape]
-        all_new_options = Enum.sort(Utils.cartesian_product(new_shape))
+        all_new_options = Enum.sort_by(Utils.cartesian_product(new_shape), fn(l) -> {Enum.sum(l), l} end)
         {current, remaining} = List.pop_at(all_new_options, 0)
 
         {current, %Counter{shape: new_shape, options: remaining}}
--- test/counter_test.exs
+++ test/counter_test.exs
@@ -26,4 +26,23 @@ defmodule CounterTest do
     {current, counter} = Counter.increment(counter)
     assert current == [0, 0, 1]
   end
+
+
+  test "increments in distance shell order then by position order" do
+    counter = Counter.new()
+    {current, counter} = Counter.increment(counter)
+    {current, counter} = Counter.increment(counter)
+    {current, counter} = Counter.increment(counter)
+    {current, counter} = Counter.increment(counter)
+    {current, counter} = Counter.increment(counter)
+    assert current == [0, 0, 0]
+    {current, counter} = Counter.increment(counter)
+    assert current == [0, 0, 1]
+    {current, counter} = Counter.increment(counter)
+    assert current == [0, 1, 0]
+    {current, counter} = Counter.increment(counter)
+    assert current == [1, 0, 0]
+    {current, counter} = Counter.increment(counter)
+    assert current == [0, 1, 1]
+  end
 end
