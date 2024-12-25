132990f576e7e0cafc7d81938142df18e46ca019
michaelrauh <michaelrauh@ymail.com>
2024-12-25 11:30:33 -0500

    change test to verify set semantics

--- test/context_keeper_test.exs
+++ test/context_keeper_test.exs
@@ -8,6 +8,7 @@ defmodule ContextKeeperTest do
 
     test "it can store a set of values which may or may not be new" do
       ContextKeeper.add(Pair.new("a", "b"))
+      ContextKeeper.add(Pair.new("a", "b"))
       ContextKeeper.add(Pair.new("c", "d"))
       assert ContextKeeper.get() == [Pair.new("a", "b"), Pair.new("c", "d")]
       ContextKeeper.stop()
