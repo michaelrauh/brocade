6d28cba8c21084c3f543bb7cff1982a65c496a7a
michaelrauh <michaelrauh@ymail.com>
2025-01-03 15:58:57 -0500

    first thoughts on counter updates for over

--- lib/counter.ex
+++ lib/counter.ex
@@ -9,43 +9,63 @@ defmodule Counter do
     :ets.delete(@table_name)
   end
 
-  def increment(shape, current_position) do
-    case :ets.lookup(@table_name, {shape, current_position}) do
-      [] ->
-        {new_shape, new_position} = get_next(shape, current_position)
-        shell = Enum.sum(new_position)
-        :ets.insert(@table_name, {{shape, current_position}, {new_shape, new_position, shell}})
-        {new_shape, new_position, shell}
+  # def increment(shape, current_position) do
+  #   case :ets.lookup(@table_name, {shape, current_position}) do
+  #     [] ->
+  #       {new_shape, new_position} = get_next(shape, current_position)
+  #       shell = Enum.sum(new_position)
+  #       :ets.insert(@table_name, {{shape, current_position}, {new_shape, new_position, shell}})
+  #       {new_shape, new_position, shell}
 
-      [{{_shape, _current_position}, {new_shape, new_position, shell}}] ->
-        {new_shape, new_position, shell}
-    end
-  end
+  #     [{{_shape, _current_position}, {new_shape, new_position, shell}}] ->
+  #       {new_shape, new_position, shell}
+  #   end
+  # end
 
   def get_next(shape, current_position) do
-    options =
-      if shape == [2, 2] do
-        shape
-        |> sorted_cartesian()
+    possibilities = sorted_cartesian(shape)
+    current = Enum.find_index(possibilities, fn x -> x == current_position end)
+    desired = Enum.at(possibilities, current + 1)
+
+    if desired == nil do
+      if Enum.all?(shape, fn x -> x == 2 end) do
+        # up on base
+        new_shape = [2 | shape]
+        new_possibilities = sorted_cartesian(new_shape)
+        used_possibilities = Enum.map(possibilities, fn x -> [0 | x] end)
+
+        desired =
+          new_possibilities
+          |> Enum.reject(fn x -> Enum.member?(used_possibilities, x) end)
+          |> List.first()
+
+        {:up, new_shape, desired}
       else
-        shape
-        |> sorted_cartesian()
-        |> Enum.reject(&(hd(&1) == 0))
+        # over - later do both
+        magnitudes = Enum.uniq(shape)
+
+        locations =
+          Enum.map(magnitudes, fn mag ->
+            Enum.find_index(shape, fn shape_member -> shape_member == mag end)
+          end)
+
+        new_shapes =
+          Enum.map(locations, fn location ->
+            List.update_at(shape, location, fn x -> x + 1 end)
+          end)
+
+        next_positions =
+          Enum.map(new_shapes, fn new_shape ->
+            sorted_cartesian(new_shape)
+            |> Enum.reject(fn x -> Enum.member?(possibilities, x) end)
+            |> List.first()
+          end)
+
+        shapes_and_positions = Enum.zip(new_shapes, next_positions)
+        {:over, shapes_and_positions}
       end
-
-    index = Enum.find_index(options, &(&1 == current_position))
-
-    if index == length(options) - 1 do
-      new_shape = [2 | shape]
-
-      new_options =
-        new_shape
-        |> sorted_cartesian()
-        |> Enum.reject(&(hd(&1) == 0))
-
-      {new_shape, hd(new_options)}
     else
-      {shape, Enum.at(options, index + 1)}
+      {:same, shape, desired}
     end
   end
 
--- test/counter_test.exs
+++ test/counter_test.exs
@@ -4,26 +4,41 @@ defmodule CounterTest do
   alias Counter
 
   test "can be incremented, skipping all already visited while increasing dimensionality and returning shell" do
-    Counter.start()
-    {_, next_position, 1} = Counter.increment([2, 2], [0, 0])
-    assert next_position == [0, 1]
-    {_, next_position, 1} = Counter.increment([2, 2], next_position)
-    assert next_position == [1, 0]
-    {_, next_position, 2} = Counter.increment([2, 2], next_position)
-    assert next_position == [1, 1]
-    {shape, next_position, 1} = Counter.increment([2, 2], next_position)
-    assert next_position == [1, 0, 0]
-    assert shape == [2, 2, 2]
-    {shape, next_position, 2} = Counter.increment(shape, next_position)
-    assert next_position == [1, 0, 1]
-    assert shape == [2, 2, 2]
+    # {:same, [2, 2], [0, 1]} = Counter.get_next([2, 2], [0, 0])
+    # {:same, [2, 2], [1, 0]} = Counter.get_next([2, 2], [0, 1])
+    # {:same, [2, 2], [1, 1]} = Counter.get_next([2, 2], [1, 0])
+    # {:up, [2, 2, 2], [1, 0, 0]} = Counter.get_next([2, 2], [1, 1])
+    Counter.get_next([2, 3], [1, 2])
+    # a b (0 0) (0 1)  | (0 0 0) (0 0 1)
+    # c d (1 0) (1 1)  | (0 1 0) (0 1 1)
 
-    Counter.stop()
+    # e f              | (1 0 0) (1 0 1)
+    # g h              | (1 1 0) (1 1 1)
   end
 
-  # a b (0 0) (0 1)  | (0 0 0) (0 0 1)
-  # c d (1 0) (1 1)  | (0 1 0) (0 1 1)
+  # test "can expand any dimension in size and fill in there" do
+  #   Counter.start()
+  #   {[2, 2], [0, 1], 1} = Counter.increment_over([2, 2], [0, 0])
+  #   {[2, 2], [1, 0], 1} = Counter.increment_over([2, 2], [1, 0])
+  #   {[2, 2], [1, 1], 2} = Counter.increment_over([2, 2], [1, 1])
+  #   {[2, 3], [0, 2], 2} = Counter.increment_over([2, 2], [1, 1])
+  #   {[2, 3], [1, 2], 3} = Counter.increment_over([2, 2], [1, 1])
 
-  # e f              | (1 0 0) (1 0 1)
-  # g h              | (1 1 0) (1 1 1)
+  #   Counter.stop()
+  #   # [2, 2]
+  #   # a b (0 0) (0 1)
+  #   # c d (1 0) (1 1)
+
+  #   # [2, 3]
+  #   # a b e (0 0) (0 1) (0 2)
+  #   # c d f (1 0) (1 1) (1 2)
+  #
+  #   # [3, 3]
+  #   # a b c (0, 0) (0, 1) (0, 2)
+  #   # d e f (1, 0) (1, 1) (1, 2)
+  #   # g h i (2, 0) (2, 1) (2, 2)
+  # end
+
+  # does it only need to do up on base? - yes
+  # todo combine and make sure there are no straightforward or rotational duplicates
 end
