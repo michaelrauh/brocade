aa71eb57b66c0b8985e2ef3657395513734fee58
michaelrauh <michaelrauh@ymail.com>
2025-01-03 18:43:16 -0500

    reduce redundancy in counter

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,11 +1,11 @@
 defmodule Counter do
   @table_name :counter
 
-  def start() do
+  def start do
     :ets.new(@table_name, [:named_table, :set, :public])
   end
 
-  def stop() do
+  def stop do
     :ets.delete(@table_name)
   end
 
@@ -15,67 +15,65 @@ defmodule Counter do
         {type, results} = get_next(shape, current_position)
 
         results_with_shells =
-          Enum.map(results, fn {new_shape, new_position} ->
-            shell = Enum.sum(new_position)
-            {new_shape, new_position, shell}
-          end)
+          for {new_shape, new_position} <- results do
+            {new_shape, new_position, Enum.sum(new_position)}
+          end
 
         :ets.insert(@table_name, {{shape, current_position}, {type, results_with_shells}})
         {type, results_with_shells}
 
-      [{{_shape, _current_position}, {type, results_with_shells}}] ->
+      [{{_shape, _pos}, {type, results_with_shells}}] ->
         {type, results_with_shells}
     end
   end
 
   def get_next(shape, current_position) do
     possibilities = sorted_cartesian(shape)
-    current = Enum.find_index(possibilities, fn x -> x == current_position end)
-    desired = Enum.at(possibilities, current + 1)
+    current_index = Enum.find_index(possibilities, &(&1 == current_position))
 
-    if desired == nil do
-      # over - later do both
-      magnitudes = Enum.uniq(shape)
+    case Enum.at(possibilities, current_index + 1) do
+      nil ->
+        over_list = over_shapes(shape, possibilities)
 
-      locations =
-        Enum.map(magnitudes, fn mag ->
-          Enum.find_index(shape, fn shape_member -> shape_member == mag end)
-        end)
+        if Enum.all?(shape, &(&1 == 2)) do
+          up = up_shape(shape, possibilities)
+          {:both, [up | over_list]}
+        else
+          {:over, over_list}
+        end
 
-      new_shapes =
-        Enum.map(locations, fn location ->
-          List.update_at(shape, location, fn x -> x + 1 end)
-        end)
-
-      next_positions =
-        Enum.map(new_shapes, fn new_shape ->
-          sorted_cartesian(new_shape)
-          |> Enum.reject(fn x -> Enum.member?(possibilities, x) end)
-          |> List.first()
-        end)
-
-      shapes_and_positions = Enum.zip(new_shapes, next_positions)
-
-      if Enum.all?(shape, fn x -> x == 2 end) do
-        # up on base
-        new_shape = [2 | shape]
-        new_possibilities = sorted_cartesian(new_shape)
-        used_possibilities = Enum.map(possibilities, fn x -> [0 | x] end)
-
-        desired =
-          new_possibilities
-          |> Enum.reject(fn x -> Enum.member?(used_possibilities, x) end)
-          |> List.first()
-
-        result_to_add = {new_shape, desired}
-        shapes_and_positions = [result_to_add | shapes_and_positions]
-        {:both, shapes_and_positions}
-      else
-        {:over, shapes_and_positions}
-      end
-    else
-      {:same, [{shape, desired}]}
+      desired ->
+        {:same, [{shape, desired}]}
     end
+  end
+
+  defp over_shapes(shape, existing) do
+    for location <- distinct_locations(shape) do
+      new_shape = List.update_at(shape, location, &(&1 + 1))
+
+      new_pos =
+        sorted_cartesian(new_shape)
+        |> Enum.reject(&Enum.member?(existing, &1))
+        |> List.first()
+
+      {new_shape, new_pos}
+    end
+  end
+
+  defp up_shape(shape, existing) do
+    new_shape = [2 | shape]
+
+    new_pos =
+      sorted_cartesian(new_shape)
+      |> Enum.reject(&Enum.member?(Enum.map(existing, fn x -> [0 | x] end), &1))
+      |> List.first()
+
+    {new_shape, new_pos}
+  end
+
+  defp distinct_locations(shape) do
+    Enum.uniq(shape)
+    |> Enum.map(fn mag -> Enum.find_index(shape, &(&1 == mag)) end)
   end
 
   defp sorted_cartesian(shape) do
